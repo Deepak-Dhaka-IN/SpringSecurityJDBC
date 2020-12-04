a5bf8a0b69ddc03bb7b2032494f6a873754e8dc4
Deepak Dhaka <deepak.dhaka@hcl.com>
2020-12-04 13:48:23 +0530

    Spring H2 Database Authentication Authorization

---
+++ src/main/java/com/dhaka/Sprignsecurityjdbc/SprignSecurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.dhaka.Sprignsecurityjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprignSecurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprignSecurityJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhaka/Sprignsecurityjdbc/SprignSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhaka.Sprignsecurityjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprignSecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
